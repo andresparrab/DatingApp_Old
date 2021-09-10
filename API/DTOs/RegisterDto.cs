c415faef7270f18c028783ab3e3c30c932a19b23
andresparrab <andres.parrab@gmail.com>
2021-09-10 19:42:58 +0200

    DTOs

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -2,6 +2,7 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
+        // DataObjects
         public string UserName { get; set; }
         public string Password { get; set; }     
     }
