6decaf43dab220adcc7b8f7eb0a8b9697b9e2f3e
andresparrab <andres.parrab@gmail.com>
2021-09-10 20:53:30 +0200

    Friday session

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -29,14 +29,39 @@ namespace API.Controllers
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
+
+            // Add user for tracking in the database but not save it yet. the same way of commit before push
             _context.Users.Add(user);
+            //Save it for real in the database
             await _context.SaveChangesAsync();
             return user;
         }
 
+        //Checks if the user exits in the Database.
         private async Task<bool> UserExists (string UserName)
         {
             return await _context.Users.AnyAsync(dbName => dbName.UserName == UserName.ToLower());
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<AppUser>>Login(LoginDto loginDto)
+        {
+            // Get the single user that match the login from the database
+            var user= await _context.Users
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+            //If not user found in the database return Unauthorized error
+            if (user ==null) return Unauthorized("Invalid username");
+            // Initialize a new instans for the hash with the encryption key
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            // hash the login Password for comparation to the on in the db
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            // Compare the password byte by byte
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i])
+                    return Unauthorized("Invalid Password");
+            }
+            return user;
         }
     }
 }
--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -2,12 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
-    public class RegisterDto
+    public class LoginDto
     {
-        // Data Transfer Objects
         [Required]
         public string UserName { get; set; }
         [Required]
-        public string Password { get; set; }     
+        public string Password { get; set; }
     }
 }
